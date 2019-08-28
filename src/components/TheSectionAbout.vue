2d43039ae50657451b81272605925c32870bdd02
Johnathan Irvin <IrvinJohnathan@gmail.com>
2019-08-28 00:53:06 -0400

    Added section image for parallax effect on the about section. WIP: Some minor linting.

--- src/components/TheSectionAbout.vue
+++ src/components/TheSectionAbout.vue
@@ -8,7 +8,29 @@ import  Section from "./Section";
 export default {
     name: "TheSectionAbout",
     components: {
-        Section
-    }
+        Section,
+    },
 };
 </script>
+
+<style scoped>
+    #about {
+        position: relative;
+        z-index: 2;
+    }
+
+    #about::after {
+        content: "";
+        background: url(../assets/img2.jpg) no-repeat center center;
+        background-size: cover;
+        background-attachment: fixed;
+        opacity: 0.45;
+
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        position: absolute;
+        z-index: 1;
+    }
+</style>
