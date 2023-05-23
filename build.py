149591e19448c9813cf08bc9288768cd548db8d1
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-05-22 23:29:56 -0400

    feat(build): Build copies all subdirectories.
    * Cleanup.

--- build.py
+++ build.py
@@ -1,62 +1,25 @@
 #!/usr/bin/env python3
 import os
 import shutil
+from typing import Callable
 
 
-def get_output_directory(name: str) -> str:
+def copy_and_apply_func(src_dir: str, dst_dir: str, func: Callable[[str], None]) -> None:
     """
-    Get the output directory for the build. If the directory does not exist,
-    it will be created.
-
-    Returns:
-        str: The output directory.
-    """
-    if not os.path.exists(name):
-        os.mkdir(name)
-    
-    return os.path.abspath(name)
-
-def get_source_files() -> list[str]:
-    """
-    Get a list of all the source files in the src directory.
-
-    Returns:
-        list[str]: A list of paths to the source files.
-    """
-    return [
-        os.path.join("src", item)
-        for item in os.listdir("src")
-    ]
-
-def get_output_file(src_file: str, output_dir: str) -> str:
-    """
-    Get the output file for a source file. The output file will be in the
-    provided output directory.
+    Copy all the files, and directories from the source directory to the destination directory.
 
     Args:
-        src_file (str): The path to the source file.
-        output_dir (str): The path to the output directory.
-
-    Returns:
-        str: The path to the output file.
-    """
-    return os.path.join(
-        get_output_directory(output_dir),
-        os.path.basename(src_file)
-    )
-
-def get_pages() -> list[str]:
-    """
-    Get a list of all the pages in the src directory.
-
-    Returns:
-        list[str]: A list of paths to the pages.
-    """
-    return [
-        os.path.join("src", item)
-        for item in os.listdir("src")
-        if item.endswith(".html") and item != "layout.html"
-    ]
+        src_dir (str): The source directory.
+        dst_dir (str): The destination directory.
+        func (Callable[[str], None]): The function to apply to each file.
+    """    
+    for root, _, files in os.walk(src_dir):
+        for file in files:
+            src_path = os.path.join(root, file)
+            dst_path = os.path.join(dst_dir, file)
+            os.makedirs(os.path.dirname(dst_path), exist_ok=True)
+            shutil.copy2(src_path, dst_path)
+            func(dst_path)
 
 def get_layout() -> str:
     """
@@ -79,17 +42,6 @@ def read_file(path: str) -> str:
     """
     with open(path) as f:
         return f.read()
-    
-def write_file(path: str, content: str) -> None:
-    """
-    Write the content to a file.
-
-    Args:
-        path (str): The path to the file.
-        content (str): The content to write to the file.
-    """
-    with open(path, "w") as f:
-        f.write(content)
 
 def copy_file(src: str, dest: str) -> None:
     """
@@ -114,7 +66,6 @@ def delete_directory(path: str) -> None:
     """
     if os.path.exists(path):
         shutil.rmtree(path)
-
 
 def inject(template: str, content: str, tag="{% body %}") -> str:
     """
@@ -154,24 +105,20 @@ def main() -> None:
     )
 
     delete_directory("dist")
-
-    for page in get_pages():
-        write_file(
-            get_output_file(
-                src_file=page,
-                output_dir="dist"
-            ),
-            minimize(
-                inject(
-                    layout,
-                    read_file(page),
-                    tag="{% body %}"
-                )
+    copy_and_apply_func(
+        src_dir="src",
+        dst_dir="dist",
+        func=lambda path: minimize(
+            inject(
+                layout,
+                read_file(path),
+                tag="{% body %}"
             )
         )
+    )
 
-        copy_file("CNAME", "dist/CNAME")
-        copy_file("favicon.ico", "dist/favicon.ico")
+    copy_file("CNAME", "dist/CNAME")
+    copy_file("favicon.ico", "dist/favicon.ico")
 
 if __name__ == "__main__":
     main()
