9327fe50b2f3c0e47bbae4bbfb63c80ea9e818eb
Johnathan Irvin <irvinjohnathan@gmail.com>
2023-06-06 17:37:16 -0400

    test(build): Test copy_file

---
+++ tests/test_build.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2021-2023 Johnathan P. Irvin
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from unittest.mock import call, mock_open, patch
+
+import pytest
+
+from build import copy_file, read_file
+
+
+def test_read_file():
+    """
+    Test the read_file function.
+    """
+    with patch("build.open", mock_open(read_data="data")) as mock_file:
+        assert read_file("path") == "data"
+        mock_file.assert_called_once_with("path")
+
+def test_copy_file():
+    """
+    Test the copy_file function.
+    """
+    with patch("build.open", mock_open()) as mock_file:
+        copy_file("src", "dst")
+
+        mock_file.assert_has_calls(
+            [
+                call("src", "rb"),
+                call("dst", "wb"),
+            ],
+            any_order=True
+        )
